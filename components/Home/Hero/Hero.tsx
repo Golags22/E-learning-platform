9ec80f4addc32cff874d50bd5304b2d73dfcf76f
Emmanuel Atuyota Golagha <153190928+Golags22@users.noreply.github.com>
2024-09-25 16:54:47 +0100

    Update Hero.tsx

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import Heroimage from './Heroimage'
 
 const Hero = () => {
   return (
-    <div className='w-full pt-[4vh] md:pt-[12vh] h-screen bg-red-950'>
+    <div className='w-full pt-[4vh] md:pt-[12vh] h-screen bg-pink-950'>
         <div className="flex justify-center flex-col w-4/5 h-full mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-12">
                  <Herocontent/>
